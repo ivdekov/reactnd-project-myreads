f924728c6ef823896e92ce0702beff12267cd621
ivdekov <ivdekov@gmail.com>
2017-07-27 15:55:07 -0500

    feat: Implementrouting with react-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
+import ListBooks from './ListBooks';
+import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
   state = {
@@ -14,7 +17,12 @@ class BooksApp extends React.Component {
   };
 
   render() {
-    return <div className="app" />;
+    return (
+      <div className="app">
+        <Route exact path="/" component={ListBooks} />
+        <Route path="/search" component={SearchBooks} />
+      </div>
+    );
   }
 }
 
