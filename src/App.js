989a67e426aebf3a8ab1a350b35ca0d80ed902de
ivdekov <ivdekov@gmail.com>
2017-07-27 17:16:39 -0500

    feat: Add state, pass down to ListBooks component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,29 @@ import ListBooks from './ListBooks';
 import SearchBooks from './SearchBooks';
 
 class BooksApp extends React.Component {
-  state = {};
-
+  state = {
+    shelves: [
+      { id: 'currentlyReading', title: 'Currently Reading' },
+      { id: 'wantToRead', title: 'Want to Read' },
+      { id: 'read', title: 'Read' }
+    ],
+    books: []
+  };
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      console.log(books);
+      this.setState({ books });
+    });
+  }
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={ListBooks} />
+        <Route
+          exact
+          path="/"
+          render={() =>
+            <ListBooks shelves={this.state.shelves} books={this.state.books} />}
+        />
         <Route path="/search" component={SearchBooks} />
       </div>
     );
