86042d2ead65420860c5780b8f27cfc4c922b72f
ivdekov <ivdekov@gmail.com>
2017-07-30 08:59:54 -0500

    feat: Set correct shelf for books from search results

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -9,17 +9,30 @@ class SearchBooks extends Component {
     books: []
   };
   updateQuery = query => {
-    this.setState({ query: query.trim() });
+    this.setState({ query });
     this.searchBooks(query);
   };
   searchBooks = query => {
     BooksAPI.search(query, 20).then(books => {
-      if (!books.error) {
-        return this.setState({ books });
+      if (Array.isArray(books)) {
+        this.setBookShelves(books);
       }
     });
   };
+  setBookShelves(books) {
+    const correctlyShelvedBooks = books.map(searchBook => {
+      const myBook = this.props.books.filter(
+        propBook => propBook.id === searchBook.id
+      )[0];
+      if (myBook) {
+        searchBook.shelf = myBook.shelf;
+      }
+      return searchBook;
+    });
+    this.setState({ books: correctlyShelvedBooks });
+  }
   render() {
+    console.log(this.state.books);
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -37,7 +50,13 @@ class SearchBooks extends Component {
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {this.state.books.map(book => <Book key={book.id} book={book} />)}
+            {this.state.books.map(book =>
+              <Book
+                key={book.id}
+                book={book}
+                onShelfChange={this.props.updateBook}
+              />
+            )}
           </ol>
         </div>
       </div>
